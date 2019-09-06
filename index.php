42d162e421c50c4ec9f19672bfa5fd3aa1a2a240
BABUNI <46104458+sairbabu58@users.noreply.github.com>
2019-09-06 23:32:14 +0530

    updated

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p> Hello World [v-007] </p>'; ?> 
- </body>
+<title> Demo Application</title>
+
+<body>
+<h1> 
+<marquee direction="right" behavior="alternate" scrollamount=5 bgcolor=red> <b><i> - Demo Application - </b></i></marquee>
+</h1>
+<img src="C:\Users\BabuniRout\Desktop\IBM.PNG" width="60%" height="60%">
+</body>
+
+<!---
+<body>
+<h1> 
+<marquee direction="right" behavior="alternate" scrollamount=5 bgcolor=red> <b><i> - Demo Application - </b></i></marquee>
+</h1>
+<img src="C:\Users\BabuniRout\Desktop\IBM-redhat.PNG" width="60%" height="60%">
+</body>
+--->
 </html>
